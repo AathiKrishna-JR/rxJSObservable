8985714beaacdb50683f1952099b28650bbfe1b3
AathiKrishna-JR <aathi3612@gmail.com>
2025-01-29 12:20:47 +0530

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, DestroyRef, OnInit, effect, inject, signal } from '@angular/core';
+import { Component, DestroyRef, OnInit, computed, effect, inject, signal } from '@angular/core';
 import {interval, map} from 'rxjs';
 @Component({
   selector: 'app-root',
@@ -8,12 +8,18 @@ import {interval, map} from 'rxjs';
 export class AppComponent implements OnInit {
   private destroyRef = inject(DestroyRef);
   clickcount = signal(0);
+  interval = signal(0);
+  doubbleInterval = computed(() => this.interval()*2);
   constructor(){
     effect(() => {
       console.log(`Clicked button ${this.clickcount()} times`);
     })
   }
   ngOnInit(): void {
+
+    setInterval(() => {
+      this.interval.update(prev => prev + 1);
+    },1000);
 
       // const subscription =interval(1000).pipe(
       //   map((val) => val * 2)
