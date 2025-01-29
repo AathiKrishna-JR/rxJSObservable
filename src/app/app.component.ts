0199462b79a5de0218c1b1ac515f357d36d2c3f0
AathiKrishna-JR <aathi3612@gmail.com>
2025-01-29 11:52:09 +0530

    map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import {interval} from 'rxjs';
+import { Component, DestroyRef, OnInit, inject } from '@angular/core';
+import {interval, map} from 'rxjs';
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
+  private destroyRef = inject(DestroyRef);
   ngOnInit(): void {
-      interval(1000).subscribe({
+      const subscription =interval(1000).pipe(
+        map((val) => val * 2)
+      ).subscribe({
         next:(val) => console.log(val),
       });
-  }
+
+      this.destroyRef.onDestroy(()=>{
+              subscription.unsubscribe();
+        });
+    }
 
 }
